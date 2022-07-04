08e3526b211e851b3fb2f20c0037d9dd8a36ba12
Kenneth Lee <kennethlee@fortinet.com>
2022-07-04 16:35:09 -0400

    docs(example): adds short-form and break-out into individual endpoints without handler helper

--- README.md
+++ README.md
@@ -14,6 +14,24 @@ DELETE /resources/:resource
 
 Also has generators for associated endpoints, i.e. `GET /resources/:resource/children/:child`
 
+Normal errors look like:
+```json
+{
+    "message": "my error here"
+}
+```
+
+
+Validation errors looks like this:
+```json
+{
+    "message": "validation errors",
+    "errors": {
+        "name": "required",
+        "<field>": "<validation error>"
+    }
+}
+```
 
 Developers
 ----------
--- example/owners.go
+++ example/owners.go
@@ -11,6 +11,17 @@ func init() {
 	owners.GET("/:owner", ownerHandlers.Fetch, ownerHandlers.Render)
 	owners.PUT("/:owner", ownerHandlers.Fetch, ownerHandlers.Update, ownerHandlers.Render)
 	owners.DELETE("/:owner", ownerHandlers.Delete)
+
+	// Short form of the above would be:
+	// ownerHandlers.Register(app, "/owners")
+
+	// Alternatively you can use the following:
+	// var owner = generator.New(db, Owner{}, "owner")  // Typically goes top of file
+	// owners.GET("", owner.List())
+	// owners.POST("", owner.Create(), owner.Render())
+	// owners.GET("/:owner", owner.Fetch(), owner.Render())
+	// owners.PUT("/:owner", owner.Fetch(), owner.Update(), owner.Render())
+	// owners.DELETE("/:owner", owner.Delete())
 }
 
 func mergeOwners(src, dest interface{}) {
